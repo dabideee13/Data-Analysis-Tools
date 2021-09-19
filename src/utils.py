91fec94f7bc8d18a923be45bd0933c56364d2a1c
dabideee13 <d.e.magno13@gmail.com>
2021-09-19 17:50:55 +0800

    Update: add annotation factory for type hints

--- src/utils.py
+++ src/utils.py
@@ -4,7 +4,8 @@
 My tools for data analysis
 """
 
-from typing import Dict, Tuple, Callable, Any, List
+from pathlib import Path
+from typing import Annotated, Dict, Tuple, Callable, Any, List
 import re
 
 import numpy as np
@@ -12,8 +13,11 @@ import pandas as pd
 import plotly.graph_objects as go
 
 
-def pipe(raw_input: Any, *functions: Callable[[Any], Any],
-         **functions_with_args: Callable[[Any], Any]) -> Any:
+def pipe(
+    raw_input: Any,
+    *functions,
+    **functions_with_args
+) -> Any:
     """
     Creates a pipeline (or chain) for every function. Basically,
     this function initially accepts a data then passes it to the next
@@ -173,3 +177,34 @@ def word_tokenize(text: str) -> List[str]:
     second_pattern = r'W+^[\s+]'
     new_text = re.sub(second_pattern, '', text)
     return re.findall(first_pattern, new_text)
+
+
+def get_dataPath(filename: str, directory: str = "raw") -> Path:
+    data_dir = Path.joinpath(Path.cwd().parent, "data/")
+    return Path.joinpath(data_dir, directory, filename)
+
+
+def get_data(filename: str, directory: str = "raw") -> str:
+    file = get_dataPath(filename, directory)
+    with open(file, encoding="utf-8") as f:
+        data = f.read()
+    return data
+
+
+class AnnotationFactory:
+    """Copied from RealPython
+
+    Link: https://realpython.com/python39-new-features/
+    """
+
+    def __init__(self, type_hint):
+        self.type_hint = type_hint
+
+    def __getitem__(self, key):
+        if isinstance(key, tuple):
+            return Annotated[(self.type_hint,) + key]
+        else:
+            return Annotated[self.type_hint, key]
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}({self.type_hint})"
