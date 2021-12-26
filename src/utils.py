d6e1a8052ecf6946d40b49ce49aa9200eb8a03d0
Dave Emmanuel Magno <56486609+dabideee13@users.noreply.github.com>
2021-12-26 08:14:13 +0800

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -107,46 +107,6 @@ class Utils:
         return self.get_average(self.df[indices_from].loc[indices_to])
 
 
-def pipe(raw_input: Any, *functions, **functions_with_args) -> Any:
-    """
-    Creates a pipeline (or chain) for every function. Basically,
-    this function initially accepts a data then passes it to the next
-    function, then the output passes it to the next function as input.
-
-    Args:
-        raw_input (Any): Any input, could be list, tuple, etc.
-
-    Other Parameters:
-        param1 (Callable): Any function with only one argument.
-        param2 (Callable): Any function with only one argument.
-        ...
-
-    Keyword Args:
-        key1 (Callable): Any function with one or more than one
-            arguments with arguments written as list.
-        key2 (Callable): Any function with one or more than one
-            arguments with arguments written as list.
-        ...
-
-    Returns:
-        Any: Any output as a result of the functions it goes through.
-    """
-
-    # TODO: Needs more improvement for robustness.
-    # Currently it will only work for some cases.
-    output = raw_input
-
-    if functions:
-        for function in functions:
-            output = function(output)
-
-    if functions_with_args:
-        for function, args_list in functions_with_args.items():
-            output = eval(function)(output, *args_list)
-
-    return output
-
-
 def get_freq(series: pd.Series) -> np.ndarray:
     """Get frequency distribution for a given series.
 
@@ -234,22 +194,6 @@ def plot_crosstab(cross: pd.DataFrame):
     fig = go.Figure(data)
     fig.update_layout(barmode="stack")
     return fig
-
-
-def word_tokenize(text: str) -> List[str]:
-    """Converts a text (in string format) to a list of string.
-
-    Args:
-        text (str): Raw text format, usually having a '.txt' file extension.
-
-    Returns:
-        List[str]: List of strings from the raw text.
-    """
-
-    first_pattern = r"[A-Za-z]{2,}"
-    second_pattern = r"W+^[\s+]"
-    new_text = re.sub(second_pattern, "", text)
-    return re.findall(first_pattern, new_text)
 
 
 def get_data_path(filename: str, directory: str = "raw") -> Path:
