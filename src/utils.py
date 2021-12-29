a4b8116e97be465837bea3170fcf906766f23940
Dave Emmanuel Magno <56486609+dabideee13@users.noreply.github.com>
2021-12-29 16:55:04 +0800

    Modify `get_freq_perc` function

--- src/utils.py
+++ src/utils.py
@@ -107,68 +107,15 @@ class Utils:
         return self.get_average(self.df[indices_from].loc[indices_to])
 
 
-def get_freq(series: pd.Series) -> np.ndarray:
-    """Get frequency distribution for a given series.
-
-    Args:
-        series (pd.Series): A column (series) in a DataFrame.
-
-    Returns:
-        np.ndarray: An array of frequencies for each distinct key.
-    """
-    return series.value_counts().values
-
-
-def get_perc(series: pd.Series) -> np.ndarray:
-    """Get percentage distribution for a given series.
-
-    Args:
-        series (pd.Series): A column (series) in a DataFrame.
-
-    Returns:
-        np.ndarray: An array of percentages for each distinct key.
-    """
-    return series.value_counts(normalize=True).values.round(4)
-
-
-def get_freq_perc(series: pd.Series) -> Tuple[int, float]:
-    """Get frequency and percentage distribution for a given series.
-
-    Args:
-        series (pd.Series): A column (series) in a DataFrame.
-
-    Returns:
-        tuple: A tuple containing frequency and percentage, respectively.
-    """
-    return tuple(zip(get_freq(series), get_perc(series)))
-
-
-def get_dist(series: pd.Series) -> Dict[int, Tuple[int, float]]:
-    """Get distribution for a given series.
-
-    Args:
-        series (pd.Series): A column (series) in a DataFrame.
-
-    Returns:
-        dict: A dictionary in which the key is the distinct value of
-        the data and the values of the dictionary is a tuple
-        consisting of both frequency and percentage, respecdtively.
-    """
-    return dict(zip(series.value_counts().keys().values, get_freq_perc(series)))
-
-
-def get_dist_all(df: pd.DataFrame) -> dict:
-    """Get distribution for all columns in a DataFrame.
-
-    Args:
-        df (pd.DataFrame): The whole DataFrame, if possible.
-
-    Returns:
-        dict: A dictionary for all columns, same with get_dist() function,
-            the difference lies only in that in here, it takes all
-            columns as input.
-    """
-    return {col: get_dist(df[col]) for col in df}
+def get_freq_perc(series: pd.Series) -> dict[str, tuple[int, float]]:
+    series_dist = series.value_counts()
+    series_perc = series.value_counts(normalize=True).values.round(4)
+    return dict(
+        zip(
+            series_dist.keys(), 
+            zip(series_dist.values, series_perc)
+        )
+    )
 
 
 def get_unique_df(df: pd.DataFrame, return_counts: bool = False) -> np.ndarray:
